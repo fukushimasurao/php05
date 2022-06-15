4013df3a79138a134468920f7ce7b3ec9defad87
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-06-16 00:07:12 +0900

    ブログ画面追加

--- index.php
+++ index.php
@@ -3,14 +3,13 @@ session_start();
 require_once('funcs.php');
 require_once('common/header_bar.php');
 require_once('common/head.php');
+require_once('common/footer.php');
 loginCheck();
 
-//２．データ登録SQL作成
 $pdo = db_conn();
 $stmt = $pdo->prepare('SELECT * FROM gs_content_table');
 $status = $stmt->execute();
 
-//３．データ表示
 $view = '';
 if ($status == false) {
     sql_error($stmt);
@@ -24,12 +23,14 @@ if ($status == false) {
 
 <head>
     <?= $head ?>
-    <title>管理画面</title>
+    <title>ブログ画面</title>
 </head>
 
 <body id="main">
-    <?= $nav_bar ?>
     <div class="album py-5 bg-light">
+        <figure class="text-center">
+            <h1>HOGEブログ</h1>
+        </figure>
         <div class="container">
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                 <?php foreach ($contents as $content): ?>
@@ -52,7 +53,6 @@ if ($status == false) {
                                 <small class="text-muted">更新日:<?= $content['update_time'] ?></small>
                             </div>
                             <?php endif ?>
-                            <a href="detail.php?id=<?=$content['id']?>" class="btn btn-outline-info stretched-link">編集する</a>
                         </div>
                         </div>
                     </div>
@@ -61,5 +61,6 @@ if ($status == false) {
             </div>
         </div>
     </div>
+<?= $footer ?>
 </body>
 </html>
