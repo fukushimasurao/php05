be0d2549c943b81a200557f1ec55787e6378e446
fukushima hayao <nu0640042@gmail.con>
2022-06-17 21:48:59 +0900

    indexの画像表示変更

--- admin/index.php
+++ admin/index.php
@@ -57,24 +57,20 @@ if ($status == false) {
                 <?php foreach ($contents as $content): ?>
                     <div class="col">
                         <div class="card shadow-sm">
-                        <?php if ($content['img']): ?>
-                            <img src="../images/<?=$content['img']?>" alt="" class="bd-placeholder-img card-img-top" >
-                        <?php else: ?>
                             <img src="../images/default_image/no_image_logo.png" alt="" class="bd-placeholder-img card-img-top" >
-                        <?php endif ?>
-                        <div class="card-body">
-                            <h3><?= $content['title'] ?></h3>
-                            <p class="card-text"><?= nl2br($content['content']) ?></p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <small class="text-muted">登録日:<?= $content['date'] ?></small>
+                            <div class="card-body">
+                                <h3><?= $content['title'] ?></h3>
+                                <p class="card-text"><?= nl2br($content['content']) ?></p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <small class="text-muted">登録日:<?= $content['date'] ?></small>
+                                </div>
+                                <?php if (!is_null($content['update_time'])): ?>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <small class="text-muted">更新日:<?= $content['update_time'] ?></small>
+                                </div>
+                                <?php endif ?>
+                                <a href="detail.php?id=<?=$content['id']?>" class="btn btn-outline-info stretched-link">編集する</a>
                             </div>
-                            <?php if (!is_null($content['update_time'])): ?>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <small class="text-muted">更新日:<?= $content['update_time'] ?></small>
-                            </div>
-                            <?php endif ?>
-                            <a href="detail.php?id=<?=$content['id']?>" class="btn btn-outline-info stretched-link">編集する</a>
-                        </div>
                         </div>
                     </div>
                 <!-- </a> -->
--- index.php
+++ index.php
@@ -39,23 +39,19 @@ if ($status == false) {
                 <!-- <a href="#"> -->
                     <div class="col">
                         <div class="card shadow-sm">
-                        <?php if ($content['img']): ?>
-                            <img src="images/<?= $content['img'] ?>" alt="" class="bd-placeholder-img card-img-top" >
-                        <?php else: ?>
                             <img src="images/default_image/no_image_logo.png" alt="" class="bd-placeholder-img card-img-top" >
-                        <?php endif ?>
-                        <div class="card-body">
-                            <h3><?= $content['title'] ?></h3>
-                            <p class="card-text"><?=nl2br($content['content'])?></p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <small class="text-muted">登録日:<?= $content['date'] ?></small>
+                            <div class="card-body">
+                                <h3><?= $content['title'] ?></h3>
+                                <p class="card-text"><?=nl2br($content['content'])?></p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <small class="text-muted">登録日:<?= $content['date'] ?></small>
+                                </div>
+                                <?php if (!is_null($content['update_time'])): ?>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <small class="text-muted">更新日:<?= $content['update_time'] ?></small>
+                                </div>
+                                <?php endif ?>
                             </div>
-                            <?php if (!is_null($content['update_time'])): ?>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <small class="text-muted">更新日:<?= $content['update_time'] ?></small>
-                            </div>
-                            <?php endif ?>
-                        </div>
                         </div>
                     </div>
                 <!-- </a> -->
