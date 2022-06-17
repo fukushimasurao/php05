56ba93673e39b801c919ce2ebfc23b23a7922c10
fukushima hayao <nu0640042@gmail.con>
2022-06-18 00:04:57 +0900

    微修正

--- admin/image.php
+++ admin/image.php
@@ -4,11 +4,13 @@ switch ($_SESSION['post']['image_type']) {
     case IMAGETYPE_JPEG:
     header('content-type: image/jpeg');
     break;
+
     case IMAGETYPE_PNG:
     header('content-type: image/png');
     break;
+
     case IMAGETYPE_GIF:
     header('content-type: image/gif');
     break;
-    }
+}
 echo $_SESSION['post']['image_data'];
