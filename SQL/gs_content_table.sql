0a155143b90771c33e30efb3b53ad2ecab92b16f
fukushima hayao <nu0640042@gmail.con>
2022-06-17 21:44:21 +0900

    テーブルdefault NULL変更

--- SQL/gs_content_table.sql
+++ SQL/gs_content_table.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: localhost:8889
--- 生成日時: 2022 年 6 月 16 日 15:39
+-- 生成日時: 2022 年 6 月 17 日 12:43
 -- サーバのバージョン： 5.7.32
 -- PHP のバージョン: 7.4.16
 
@@ -31,7 +31,7 @@ CREATE TABLE `gs_content_table` (
   `id` int(12) NOT NULL,
   `title` varchar(64) NOT NULL COMMENT '記事のタイトル',
   `content` varchar(256) NOT NULL COMMENT '記事の内容',
-  `img` varchar(256) NOT NULL COMMENT '画像のPATH',
+  `img` varchar(256) DEFAULT NULL COMMENT '画像のPATH',
   `date` datetime NOT NULL COMMENT '登録日',
   `update_time` datetime DEFAULT NULL COMMENT '更新日（NULL許容）'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='記事のテーブル';
@@ -41,9 +41,10 @@ CREATE TABLE `gs_content_table` (
 --
 
 INSERT INTO `gs_content_table` (`id`, `title`, `content`, `img`, `date`, `update_time`) VALUES
-(35, 'タイトルテスト', '記事内容記事内容記事内容', '', '2022-06-17 00:38:29', NULL),
+(35, '1111333', '22222222', '20220617122724_dora_8.png', '2022-06-17 00:38:29', '2022-06-17 21:40:47'),
 (36, 'タイトル２', 'きじ２', '', '2022-06-17 00:38:50', NULL),
-(37, '３３３３３', '３３３３３３', '', '2022-06-17 00:38:55', NULL);
+(37, '３３３３３', '３３３３３３', '', '2022-06-17 00:38:55', NULL),
+(38, 'asdf', 'asdf', '20220617122635_dora_15.png', '2022-06-17 21:26:35', NULL);
 
 --
 -- ダンプしたテーブルのインデックス
@@ -63,7 +64,7 @@ ALTER TABLE `gs_content_table`
 -- テーブルの AUTO_INCREMENT `gs_content_table`
 --
 ALTER TABLE `gs_content_table`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
